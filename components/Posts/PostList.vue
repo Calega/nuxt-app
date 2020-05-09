d70eecc8f8d7f6d6b31eed559fc716294eb39cce
Lucas Calegari Alves de Oliveira <lcalegari17@gmail.com>
2020-05-09 15:42:14 -0700

    Adding vuex to handle data fetching

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -1,15 +1,17 @@
-<template >
-    <section class="posts" >
-        <PostPreview id = "1" 
-                    thumbnail = "https://www.businessnewsdaily.com/images/i/000/015/216/original/salesforce.png"
-                    title = "First post"
-                    previewText = "This is my first post"
-                    :is-admin="isAdmin" />
-    </section>
+<template>
+  <section class="post-list">
+    <PostPreview
+      v-for="post in posts"
+      :key="post.id"
+      :id="post.id"
+      :is-admin="isAdmin"
+      :thumbnail="post.thumbnail"
+      :title="post.title"
+      :previewText="post.previewText" />
+  </section>
 </template>
 
 <script>
-
 import PostPreview from '@/components/Posts/PostPreview'
 
 export default {
@@ -20,19 +22,25 @@ export default {
     isAdmin: {
       type: Boolean,
       default: false
+    },
+    posts: {
+      type: Array,
+      required: true
     }
   }
 }
-
 </script>
 
+
 <style scoped>
-    .posts {
-        display: flex;
-        padding: 20px;
-        box-sizing: border-box;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: center;
-    }
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
 </style>
+
--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,41 +1,41 @@
 <template>
-    <div class="admin-post-page">
-        <section class="update-form">
-            <AdminPostForm :post="loadedPost" />
-        </section>
-    </div>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
 </template>
 
 <script>
-
 import AdminPostForm from '@/components/Admin/AdminPostForm'
 
 export default {
-    layout: 'admin',
-    components: {
-        AdminPostForm
-    },
-    data() {
-        return {
-            loadedPost: {
-                author: 'Lucas Oliveira',
-                title: 'My awesome post',
-                content: 'Yuppieee',
-                thumbnailLink: 'https://www.businessnewsdaily.com/images/i/000/015/216/original/salesforce.png'
-            }
-        }
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: 'Lucas Oliveira',
+        title: 'My awesome Post',
+        content: 'Super amazing, thanks for that!',
+        thumbnailLink: 'https://www.businessnewsdaily.com/images/i/000/015/216/original/salesforce.png'
+      }
     }
+  }
 }
 </script>
 
 <style scoped>
-    .update-form {
-        width: 90%;
-        margin: 20px auto;
-    }
-    @media (min-width: 768px) {
-        .update-form {
-            width: 500px;
-        }
-    }
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
 </style>
