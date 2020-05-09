0250cf7d17eb29eebb309bf33539500463c8e74d
Lucas Calegari Alves de Oliveira <lcalegari17@gmail.com>
2020-05-09 12:29:42 -0700

    Adming page

---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+    layout: 'admin',
+    components: {
+        AdminPostForm
+    },
+    data() {
+        return {
+            loadedPost: {
+                author: 'Lucas Oliveira',
+                title: 'My awesome post',
+                content: 'Yuppieee',
+                thumbnailLink: 'https://www.businessnewsdaily.com/images/i/000/015/216/original/salesforce.png'
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .update-form {
+        width: 90%;
+        margin: 20px auto;
+    }
+    @media (min-width: 768px) {
+        .update-form {
+            width: 500px;
+        }
+    }
+</style>
